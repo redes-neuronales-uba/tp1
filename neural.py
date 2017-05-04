e67ca58c91dc4021dd28ddb5be721634a794dc06
nico <nico@localhost.localdomain>
2017-05-04 03:51:03 -0300

    Agregado metodo para conectar al azar y algunos tests, arreglado forward.

--- neural.py
+++ neural.py
@@ -13,13 +13,13 @@ def sigmoid_prime(x):
 # Clase para una capa neuroal
 class NeuralLayer:
     # Cantidad de neuronas de la capa
-    internal_neurals_number = 0
+    neurons = 0
 
     # Pesos que se conectan a cada neurona de la capa
-    array_weights_in = None
+    weights = None
 
-    # Resultado para cada neurona de aplicar los pesos a los parametros que le van a pasar a la neurona
-    array_nodes_sumatory = None
+    # Induced local field de las neuronas (pesos x input)
+    v = None
 
     # Bias
     bias = None
@@ -28,34 +28,40 @@ class NeuralLayer:
     total_error = 0
 
     # Iniciador de neurona (digase constructor)
-    def __init__(self, internal_neurals_number, weights_in=[], bias=(0, 0)):
-        # Si no me dan pesos los pongo aleatorios
-        if (len(weights_in) == 0):
-            self.array_weights_in = np.random.rand(internal_neurals_number)
-        else:
-            n, m = np.shape(weights_in)
-            assert (m == internal_neurals_number)
-            self.array_weights_in = weights_in
-            self.bias = bias
+    def __init__(self, neurons, weights_in=[], bias=(0, 0)):
+        #n, m = np.shape(weights_in)
+        #assert (n == neurons) # el problema es que numpy no dif entre vectores columna y fila
+        self.weights = weights_in        
+        self.bias = bias
 
         # Seteo el numero de neuronas
-        self.internal_neurals_number = internal_neurals_number
+        self.neurons = neurons
 
-    # Aplica los pesos a la variable de entrada values_matrix
-    def apply_weights_to_params(self, values_matrix):
-        self.array_nodes_sumatory = np.dot(values_matrix, self.array_weights_in) + (self.bias[0] * self.bias[1])
+    # Calcula el induced local field, v y lo guarda
+    def compute_v(self, x_input):
+        self.v = np.dot(self.weights, x_input) + (self.bias[0] * self.bias[1])
 
-    # Retorna array_nodes_sumatory
-    def get_result_of_nodes_sumatory(self):
-        return self.array_nodes_sumatory
+    # Devuelve el induced local field v
+    def get_v(self):
+        return self.v
 
-    # Retorna el arreglo de pesos
-    def get_array_weights_in(self):
-        return self.array_weights_in
+    # Devuelve el output de las neuronas de esta capa
+    def get_output(self, phi):
+        return phi(self.v)
+
+    # Devuelve los pesos conectando las neuronas de esta capa con la anterior
+    def get_weights(self):
+        return self.weights
 
     # Setea el arreglo de pesos
-    def set_array_weights_in(self, a_new_array_weights_array):
-        self.array_weights_in = a_new_array_weights_array
+    def set_weights(self, new_weights):
+        self.weights = new_weights
+    
+    def set_bias(self, bias):
+        self.bias = bias
+
+    def get_bias(self, bias):
+        return self.bias
 
 
 class NeuralNetwork:
@@ -64,11 +70,12 @@ class NeuralNetwork:
     layers = None
     layers_number = None
     X = None
+    phi = None
 
     # Constructor
-    def __init__(self, eta=0.5):
+    def __init__(self, in_signals, eta=0.5, phi=sigmoid):
 
-        # Defino el coheficiente de aprendizaje
+        # Defino el coeficiente de aprendizaje
         self.eta = eta
 
         # Lista de valores de errores
@@ -77,28 +84,50 @@ class NeuralNetwork:
         # Lista de Capas
         self.layers = []
 
-        # Numero de capas
+        # Numero de capas sin contar la de input
         self.layers_number = 0
 
+        # Funcion de activacion
+        self.phi = phi
+
+        # Cantidad de seniales de entrada
+        self.in_signals = in_signals
+
+    
+    
     # Agrega una capa neuronal
-    def add_layer(self, a_neural_layer):
-        self.layers.append(a_neural_layer)
+    def add_layer(self, layer):
+        self.layers.append(layer)
         self.layers_number += 1
+
+    def connect(self): # quizas es mejor hacerlo directamente en el init?
+        assert(self.layers > 0)
+        factor = 4.0 # para que los pesos esten entre 0 y 1/factor
+        neuronas_capa_anterior = self.in_signals
+        for i in range(0, self.layers_number):
+            capa_a_conectar = self.layers[i]
+            filas = capa_a_conectar.neurons
+            columnas = neuronas_capa_anterior
+            weights = np.random.rand(filas, columnas)/factor
+            capa_a_conectar.set_weights(weights)
+            capa_a_conectar.set_bias((1, np.random.random()/factor))
+            neuronas_capa_anterior = capa_a_conectar.neurons
+
 
     # Proceso de forward_propagation para la entrada X
     def forward_propagation(self, X):
-        # Tiene que tener al menos 2 capas, la entrada y la salida
-        self.X = X
+        # Tiene que tener al menos 1 capa ademas de la salida
+        self.X = X # sirve para algo?
         in_parameters = X
-        assert (self.layers_number >= 2)
+        assert (self.layers_number >= 2) # creo que deberia ser 1
         for i in range(0, len(self.layers)):
-            self.layers[i].apply_weights_to_params(in_parameters)
-            in_parameters = sigmoid(self.layers[i].get_result_of_nodes_sumatory())
+            self.layers[i].compute_v(in_parameters)
+            in_parameters = self.layers[i].get_output(self.phi)
         return in_parameters
 
     def get_total_error(self, y_expected):
         output_layer = self.layers[self.layers_number-1]
-        errors = (1/2) * ((sigmoid(output_layer.get_result_of_nodes_sumatory()) - y_expected)**2)
+        errors = (1/2) * ((output_layer.get_output(self.phi) - y_expected)**2)
         return np.sum(errors)
 
 
@@ -114,17 +143,17 @@ class NeuralNetwork:
             delta_3 = np.multiply(diff_y_expected_and_estimated, f_prime_on_z3)
             a_2 = sigmoid(self.layers[0].get_result_of_nodes_sumatory())
             djdW2 = a_2.T * delta_3
-
+            
             # Calculo djdW2
             W2 = self.layers[1].get_array_weights_in()
             z2 = self.layers[0].get_result_of_nodes_sumatory()
             f_prime_on_z2 = sigmoid_prime(z2)
             delta_2 = np.dot(delta_3, W2.T) * f_prime_on_z2
             djdW1 = np.dot(self.X.T, delta_2)
-
+            
             # Actualizo los pesos de cada capa
             a_array_of_new_weights_1 = self.layers[0].get_array_weights_in() - self.eta * djdW1
             a_array_of_new_weights_2 = self.layers[1].get_array_weights_in() - self.eta * djdW2
             self.layers[0].set_array_weights_in(a_array_of_new_weights_1)
             self.layers[1].set_array_weights_in(a_array_of_new_weights_2)
-        return a_array_of_new_weights_1, a_array_of_new_weights_2
+            return a_array_of_new_weights_1, a_array_of_new_weights_2
--- tests.py
+++ tests.py
@@ -27,21 +27,23 @@ training_data_xor = [
 
 
 def test_neural_network_back_propagation_xor():
-    a_neural_1 = neural.NeuralLayer(3, np.array([[0.8, 0.4, 0.3], [0.2, 0.9, 0.5]]))
-    a_neural_2 = neural.NeuralLayer(1, np.array([[0.3], [0.5], [0.9]]))
+    b1 = (1, 0)
+    b2 = (1, 0)
+    a_neural_1 = neural.NeuralLayer(3, np.array([[0.1, 0.2], [0.2, 0.01], [0.2, 0.02]]), b1)
+    a_neural_2 = neural.NeuralLayer(1, np.array([[0.1, 0.05, 0.1]]), b2)
     n = neural.NeuralNetwork()
     n.add_layer(a_neural_1)
     n.add_layer(a_neural_2)
 
-    for i in range(1, 100):
+    for i in range(1, 2):
         x, y_expected = random.choice(training_data_xor)
-        n.forward_propagation(np.array([x]))
+        n.forward_propagation(np.array(x))
         n.back_propagation(np.array([y_expected]))
 
-    print(n.forward_propagation(np.array([[1, 0]])))
-    print(n.forward_propagation(np.array([[0, 1]])))
-    print(n.forward_propagation(np.array([[0, 0]])))
-    print(n.forward_propagation(np.array([[1, 1]])))
+    print n.forward_propagation(np.array([1, 0]))
+    print n.forward_propagation(np.array([0, 1]))
+    print n.forward_propagation(np.array([0, 0]))
+    print n.forward_propagation(np.array([1, 1]))
 
     return
 
@@ -59,12 +61,69 @@ def test_neural_back_propagation_example():
     x = np.array([[0.05, 0.10]])
     y_expected = np.array([[0.01, 0.99]])
     for i in range(1, 10000):
-        print(n.forward_propagation(x))
-        print(n.get_total_error(y_expected))
+        print n.forward_propagation(x)
+        print n.get_total_error(y_expected)
         n.back_propagation(y_expected)
 
 
-#TESTS
+def test_forward_mazzur():
+    # Test del ejemplo en 
+    # mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+    l1 = np.array([[0.15, 0.20], [0.25, 0.30]])
+    l2 = np.array([[0.4, 0.45], [0.50, 0.55]])
+    b1 = (1, 0.35)
+    b2 = (1, 0.6)
+    nl1 = neural.NeuralLayer(2, l1, b1)
+    nl2 = neural.NeuralLayer(2, l2, b2)
+
+    n = neural.NeuralNetwork(2)
+    n.add_layer(nl1)
+    n.add_layer(nl2)
+
+    # El valor correcto es [0.75136507, 0.772928465]
+    print n.forward_propagation(np.array([0.05, 0.10]))
+
+
+def test_connect():
+    # Testeo que las dimensiones de todas las matrices estan bien.
+    nl1 = neural.NeuralLayer(2)
+    nl2 = neural.NeuralLayer(2)
+
+    n = neural.NeuralNetwork(2)
+    n.add_layer(nl1)
+    n.add_layer(nl2)
+    n.connect()
+
+    print n.forward_propagation(np.array([0.05, 0.10]))
+
+
+def test_forward_miller():
+        # Test del ejemplo en 
+        # stevenmiller888.github.io/mind-how-to-build-a-neural-network/
+        l1 = np.array([[0.8, 0.2], [0.4, 0.9], [0.3, 0.5]])
+        l2 = np.array([[0.3, 0.5, 0.9]])
+        b1 = (0, 0)
+        b2 = (0, 0)
+        nl1 = neural.NeuralLayer(3, l1, b1)
+        nl2 = neural.NeuralLayer(1, l2, b2)
+        
+        n = neural.NeuralNetwork(2)
+        n.add_layer(nl1)
+        n.add_layer(nl2)
+
+        x = np.array([1,1])
+        # El valor correcto es 0.77 (el autor no da mas decimales)
+        print n.forward_propagation(x)
+
+# Test connect
+test_connect()
+
+# Tests forward
+#test_forward_mazzur()
+#test_forward_miller()
+
+# Test backward
 #test_neural_network_back_propagation()
-test_neural_back_propagation_example()
+#test_neural_back_propagation_example()
 #test_neural_network_back_propagation_xor()
+
